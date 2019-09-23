221ca66b9753f7f7a201eefafb5e4ca436fe1a04
JoHnRembo <happinessss57@gmail.com>
2019-09-23 21:06:56 +0300

    start commit

---
+++ lib/features/home/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+
+  static const String routeName = '/Home';
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+
+    );
+  }
+
+}
